0553049f9fa064c921be5b0802f82f47f114ac4b
jarhill0 <jarhill0@users.noreply.github.com>
2019-04-22 02:43:58 -0400

    Add rudimentary GUI

--- src/const.h
+++ src/const.h
@@ -14,16 +14,16 @@
 
 const double START_FUEL = 100;
 const double START_X_POS = -100;
-const double START_Y_POS = 800;
+const double START_Y_POS = 500;
 const double START_ROT = M_PI / 2.0;
 const double START_X_VEL = 0;
 const double START_Y_VEL = 0;
 const double START_ANG_VEL = 0;
 
-const double GRAV_ACC = 0.1654 / 60;
-const double MAIN_THRUST_ACC = 1 / 3.0;
-const double SIDE_THRUST_ACC = M_PI / 20.0;
+const double GRAV_ACC = 0.01;
+const double MAIN_THRUST_ACC = 0.015;
+const double SIDE_THRUST_ACC = 0.001;
 const double MAIN_THRUST_FUEL_USE = 0.1;
-const double SIDE_THRUST_FUEL_USE = 0.5;
+const double SIDE_THRUST_FUEL_USE = 0.05;
 
 #endif
